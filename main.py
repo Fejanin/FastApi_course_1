e9ecb6f9f874b771eb78dc76b5dae7a00117fd35
Shvec Sergei <fejanin@mail.ru>
2023-10-24 14:24:45 +0300

    Added function for post request

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    username: str
+    message: str
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return FileResponse('index.html')
+
+@app.post("/")
+async def root(user: User):
+    '''тут мы можем с переменной user, которая в себе содержит объект класса User с соответствующими полями (и указанными типами), делать любую логику
+        - например, мы можем сохранить информацию в базу данных
+        - или передать их в другую функцию
+        - или другое'''
+    print(f'Мы получили от юзера {user.username} такое сообщение: {user.message}')  # тут мы просто выведем полученные данные на экран в отформатированном варианте
+    return user # или можем вернуть обратно полученные данные, как символ того, что данные получили, или другая логика на ваш вкус
 
 # новый роут
 @app.get("/custom")
