2d911b39b863a5824b28a2bf5459b53bd6c40d33
Shvec Sergei <fejanin@mail.ru>
2023-11-06 17:36:33 +0300

    Параметры пути

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 from fastapi.responses import FileResponse, HTMLResponse
 from pydantic import BaseModel
 from models import User
@@ -34,15 +34,26 @@ async def download_file():
     # взяли файл test_file.txt, переименовал его в testpage.html и произвели его загрузку
     return FileResponse('download_files/test_file.txt', filename='testpage.html', media_type='application/octet-stream')
 
+@app.get('/user/{id}')
+def user_id(id: int ): # будет принимать только число
+    return {'user_id': id}
+
+@app.get('/users/{name}/{age}')
+def user_name_age(name, age):
+    return {'user_name': name, 'user_age': age}
 
 
+@app.get('/us/{name}-{age}')
+def user_name_age_(name, age):
+    return {'user_name': name, 'user_age': age}
 
+@app.get('/go_to/{name}')
+def user_name(name: str = Path(min_length=3, max_length=20)): # строка с длииной от 3 до 20
+    return {'user_name__': name}
 
-
-
-
-
-
+@app.get('/phones/{phone}')
+def phones(phone: str = Path(regex="^\d{11}$")):
+    return {'user_phone': phone}
 
 
 
